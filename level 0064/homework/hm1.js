4e590a7824389c58dcd625ce659ddac8c78096fc
Rezi5 <rezidumbadze2010@gmail.com>
2025-09-25 15:46:22 +0400

    commit

---
+++ level 0064/homework/hm1.js
@@ -0,0 +1,50 @@
+//funqciebis sheqmnis sami gzagvaqvs 1)function user(){} 2)const = function(){} 3) const = user = () =>{}
+
+// const user = () =>{
+
+// }
+
+// for (let o = 10; o < 30; o++ ){
+//     console.log(o)
+// }
+
+// function square(number) {
+//   return number * number;
+// }
+
+// console.log(square(4));
+
+// const user = function(F,S){
+//     return F + S
+// }
+// console.log(user("Rezi","Dumbadze"))
+
+// function user(f){
+//     return f * 2
+// }
+// console.log(user(20))
+
+// For cikli iqmneba jer vwert for(let i = 10; i< 50; i ++) ese iqmneba for cikli es dabewdavs 10 dan 50 amde ricxvebs
+
+// for(let i = 1; i< 10; i ++)
+//     console.log(i)
+
+// for(let i = 0; i< 100; i += 5)
+//     console.log(i)
+
+// const printNumbers = (number) => {
+// for (let i = 1; i <= number; i++) {
+//     console.log(i);
+// }
+// };
+
+// printNumbers(5);
+
+const printNumbers = (number) => {
+for (let i = 0; i <= number; i+=5) {
+    console.log(i);
+}
+};
+
+printNumbers(90);
+
