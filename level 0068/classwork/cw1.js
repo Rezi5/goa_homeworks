899651de8764379070089370982a85a1af0d45f2
Rezi5 <rezidumbadze2010@gmail.com>
2025-10-09 00:29:18 +0400

    commit

---
+++ level 0068/classwork/cw1.js
@@ -0,0 +1,19 @@
+// function calculate(str) {
+
+// let words = str.split(" ");
+// let a=undefined
+// let b=undefined 
+// let op=undefined;
+
+// for (let i = 0; i < words.length; i++) {
+//     if (!isNaN(words[i])) {
+//     if (a === undefined) a = Number(words[i]);
+//     else b = Number(words[i]);
+//     }
+//     if (words[i] === "loses" || words[i] === "gains") op = words[i];
+//     }
+
+//     if (op === "loses") return a - b;
+//     if (op === "gains") return a + b;
+
+// }
---
+++ level 0068/homework/hm1.js
@@ -0,0 +1,75 @@
+// let i = 0
+// do{
+//     console.log("numbers",i)
+//     i++
+// }while(i < 6){
+
+
+// }
+
+// let i = 10;
+
+// do {
+//     console.log(i);
+//     i--;
+// } while (i >= 1);
+
+// let user = prompt("Enter a number")
+// let i = 1
+// do{
+//     console.log(i)
+//     i++;
+// }while(i <= user)
+
+// let i = 1;
+
+// do {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 20);
+
+// let user = (h) =>{
+//     return(h)
+// }
+// console.log(user("hello world"))
+
+// let user = (num1,num2) =>{
+//     return(num1+num2)
+// }
+// console.log(user(19,20))
+
+// let user = (num) =>{
+//     if(num %2 === 0)
+//         return("Even")
+//     else{
+//         return("odd")
+//     }
+// }
+// console.log(user(20))
+
+// let name = "Rezi"
+// let age = 17
+// console.log(`hello my name is ${name} and i am ${age} years old`)
+
+// let name = "Rezi"
+// let country = "Georgia"
+// let profession = "Athlete"
+// console.log(`Hello my name is ${name} i am proffesional ${profession} i am from ${country}`)
+
+// function user(item,quality,price){
+//     return`You bought ${item} for ${price} Dollars and quality is ${quality}`
+// }
+// console.log(user("shoe","Very high",200))
+
+// const user = function(name){
+//     return("hello",name)
+// }
+
+// console.log(user("Rezi"))
+
+const user = function(num1,num2){
+    return(num1 + num2)
+}
+console.log(user(27,16))
