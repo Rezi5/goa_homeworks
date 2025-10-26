83a7f7bea10494a299b97f9d04ea8983eca9c270
Rezi5 <rezidumbadze2010@gmail.com>
2025-10-26 21:08:02 +0400

    commit

---
+++ level 0072/homework/hm1.js
@@ -0,0 +1,26 @@
+// let user = document.getElementById('r')
+// user.innerHTML = "Hello world"
+
+// let user = document.querySelector('div')
+// user.innerHTML = '<h2>Hello</h2>'
+
+// let user = document.getElementsByClassName('n')
+// user[0].textContent = 'This is paragraph'
+
+// let user = document.querySelector('ul')
+// user.innerHTML = '<ul><li>apple</li></ul>'
+
+// let user = document.querySelector('div')
+// user.innerHTML = '<img src="./download.jpg" alt="">'
+
+// let user = document.querySelector('p')
+// user.innerHTML += 'world'
+
+// let user = document.querySelector('p')
+// user.textContent = 'New content updated'
+const main = document.querySelector('main');
+main.innerHTML = `
+    <div>Welcome!</div>
+    <p>This is inside the main tag.</p>
+    <button>Click me</button>
+`;
